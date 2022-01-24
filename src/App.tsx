646fb0e544fc8845b3ea09f6414b2aaedaa482a0
matheuscalaca <matheusfcalaca@gmail.com>
2022-01-25 07:20:39 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { MdCheckCircleOutline, MdCopyright } from "react-icons/md";
+
+function App() {
+  return (
+    <>
+      <div className='header'>
+        <div className='header-img'>
+          <img src={logo} className="App-logo" alt="logo" width={32} />
+        </div>
+
+        <div className='Menu'>
+          <span>Sobre</span>
+          <span>|</span>
+          <span>Login</span>
+        </div>
+      </div>
+      <div className='content'>
+        <div className='firstPage'>
+          <div className='title'>
+            <h1>Controlando sua Vida Financeira</h1>
+          </div>
+          <div className='subtitle'>
+            <h2>Ajudamos você a controlar seus gastos e receitas e ter uma visão
+              ampla da sua situação financeira. </h2>
+          </div>
+        </div>
+      </div>
+      <div className='content'>
+        <div className='page'>
+          <div className='title'>
+            <h1>O que você vai encontrar</h1>
+          </div>
+          <div className='subtitle'>
+            <h2>Principais ações disponíveis no sistema para, que você consiga controlar a sua vida financeira. E dar o primeiro passo para acabar com as dividas.</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className='contentCard'>
+        {/* Receita */}
+
+        <div className='pageCard'>
+          <div className='imgCard'>
+            <MdCheckCircleOutline />
+          </div>
+          <div className='textCard'>
+            <div className='titleCard'>
+              <h1>Gerenciar Receitas</h1>
+            </div>
+            <div className='subtitleCard'>
+              <h2>Cadastre suas receitas mensais , e gerencia seus recebíveis de forma fácil.</h2>
+            </div>
+          </div>
+        </div>
+
+        {/* Despesa */}
+
+        <div className='pageCard'>
+          <div className='imgCard'>
+            <MdCheckCircleOutline />
+          </div>
+          <div className='textCard'>
+            <div className='titleCard'>
+              <h1>Gerenciar Despesas</h1>
+            </div>
+            <div className='subtitleCard'>
+              <h2>Cadastre seus gastos diários, e controle quanto e como você gasta seu dinheiro.</h2>
+            </div>
+          </div>
+        </div>
+
+        {/* metas */}
+
+
+        <div className='pageCard'>
+          <div className='imgCard'>
+            <MdCheckCircleOutline />
+          </div>
+          <div className='textCard'>
+            <div className='titleCard'>
+              <h1>Gerenciar Metas</h1>
+            </div>
+            <div className='subtitleCard'>
+              <h2>Crie e gerencie suas metas financeiras.</h2>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className='contentFooter'>
+        <div className='titleFooter'>
+          <h1><MdCopyright /> Matheus Calaça</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
