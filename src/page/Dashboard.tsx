cdd4f2519d3822d7575f1105043b89df11a72d58
matheuscalaca <matheusfcalaca@gmail.com>
2022-01-28 09:50:33 +0800

    dash board grafico

--- src/page/Dashboard.tsx
+++ src/page/Dashboard.tsx
@@ -5,14 +5,50 @@ import Chart from 'react-google-charts';
 import HeaderDash from '../component/HeaderDash';
 import { MdModeEdit } from 'react-icons/md';
 
+
+export const dataReceita = [
+  { "id": 1, "nome": "Salario", "data": "15/12/2021", "valor": 1506.03, "descricao": "parcela do decimo terceiro salario" },
+  { "id": 1, "nome": "Salario", "data": "10/12/2021", "valor": 2872.43, "descricao": "" },
+  { "id": 2, "nome": "Venda", "data": "05/12/2021", "valor": 1500.00, "descricao": "venda do video game PS3" },
+  { "id": 3, "nome": "Salario", "data": "10/11/2021", "valor": 2872.43, "descricao": "" },
+  { "id": 4, "nome": "Salario", "data": "10/10/2021", "valor": 2872.43, "descricao": "" },
+  { "id": 5, "nome": "Salario", "data": "10/09/2021", "valor": 2872.43, "descricao": "" }
+];
+
+
+export const dataDespesa = [{ "id": 1, "nome": "Conta de Energia", "valor": 872.43, "data": "20/12/2021", "porque": "", "meioPagamento": "Boleto", "onde": "Enel", "categoria": "Habitação" },
+{ "id": 2, "nome": "almoço de trabalho", "valor": 45.21, "data": "18/12/2021", "porque": "saida com o pessoal de trabalho para almoçar", "meioPagamento": "Boleto", "onde": "", "categoria": "Restaurante" },
+{ "id": 3, "nome": "Conta de Agua", "valor": 150.10, "data": "15/12/2021", "porque": "", "meioPagamento": "Boleto", "onde": "Saneago", "categoria": "Habitação" },
+{ "id": 4, "nome": "Internet", "valor": 89.90, "data": "15/12/2021", "porque": "", "meioPagamento": "Boleto", "onde": "", "categoria": "Habitação" },
+{ "id": 5, "nome": "Presente aniversario", "valor": 72.90, "data": "10/12/2021", "porque": "Compra do presente de aniversario do Jõao", "meioPagamento": "Cartão de Credito", "onde": "Renner", "categoria": "Familia" },
+{ "id": 6, "nome": "Jantar", "valor": 372.43, "data": "09/12/2021", "porque": "Saida para jantar em familia", "meioPagamento": "Cartão de Credito", "onde": "Pizza na Brasa", "categoria": "Familia" },
+{ "id": 7, "nome": "sorvete", "valor": 22.18, "data": "03/12/2021", "porque": "compra de sorvet pois estava com vontade", "meioPagamento": "PIX", "onde": "Soreveteria Açai", "categoria": "Restaurante" }];
+
+function somaDespesa() {
+  const sum = dataDespesa.filter(item => item.valor)
+    .reduce((sum, current) => {
+      return sum + current.valor
+    }, 0);
+  return Math.round(sum * 100) / 100
+}
+
+function somaReceita() {
+  const sum = dataReceita.filter(item => item.valor)
+    .reduce((sum, current) => {
+      return sum + current.valor
+    }, 0);
+  return Math.round(sum * 100) / 100
+}
+
+
 export const data = [
   ["Task", "Hours per Day"],
-  ["receita", 1000],
-  ["gasto", 3000],
+  ["receita", somaDespesa()],
+  ["gasto", somaReceita()],
 ];
 
 export const options = {
-  pieHole: 0.4,
+  pieHole: 0.3,
 };
 
 function Dashboard() {
@@ -34,10 +70,10 @@ function Dashboard() {
           </div>
           <div className='dadosFinanceiro'>
             <div className='titleSaldo'>
-              <div>Saldo: </div> <div>1000</div> <div>R$</div>
+              <div>Saldo: </div> <div>{(somaReceita() - somaDespesa())}</div> <div>R$</div>
             </div>
             <div className='resumoFinanceiro'>
-              <span>Receita: </span> <span className='colorGreen'>1000</span> <span>/</span> <span>Despesa:</span> <span className='colorRed'>3000</span> <span>R$</span>
+              <span>Receita: </span> <span className='colorGreen'>{somaReceita()}</span> <span>/</span> <span>Despesa:</span> <span className='colorRed'>{somaDespesa()}</span> <span>R$</span>
             </div>
           </div>
         </div>
